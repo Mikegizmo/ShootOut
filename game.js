b05e6ffff1e4d177fc630ecc515c9e351b7674d0
Michael Panter <makinglife1231@gmail.com>
2023-03-31 23:28:17 -0400

    game scoring and reset

--- game.js
+++ game.js
@@ -1,24 +1,45 @@
 const canvas = document.createElement('canvas');
 const grid = 50;
+const mind = {
+  thinking: false,
+  dir: 5, 
+  count: 0
+};
+
 canvas.setAttribute('width', grid*20);
 canvas.setAttribute('height', grid*15);
 document.body.prepend(canvas);
 canvas.style.border = '1px solid black';
 const ctx = canvas.getContext('2d');
+
+const btn = document.createElement('button');
+btn.style.display = 'block';
+btn.textContent = 'Turn On';
+document.body.prepend(btn);
+btn.style.backgroundColor = 'red';
+btn.style.color = 'white';
+btn.style.padding = '10px';
+
+btn.addEventListener('click', (e) => {
+  if(!mind.thinking) {
+    mind.thinking = true;
+    btn.textContent = 'Turn Off';
+    btn.style.backgroundColor = 'green';
+  } else {
+    mind.thinking = false;
+    btn.textContent = 'Turn On';
+    btn.style.backgroundColor = 'red';
+  }
+})
+
 const players = [{
-  x:canvas.width/2 + (grid*2),
-  y:canvas.height/2,
-  size:30,
-  speed:3,
+  x:canvas.width/2 + (grid * 6),
   color:'red',
-  cooldown:0
+  pos:canvas.width / 2 + (canvas.width / 4)
 }, {
-  x:canvas.width/2 - (grid*2),
-  y:canvas.height/2,
-  size:30,
-  speed:3,
+  x:canvas.width/2 - (grid * 6),
   color:'blue',
-  cooldown:0
+  pos:canvas.width / 4
 }];
 
 const game = {
@@ -26,6 +47,7 @@ const game = {
   bullets:[],
   bulletSpeed:5
 };
+
 const keyz = {
   ArrowLeft:false,
   ArrowRight:false,
@@ -36,6 +58,22 @@ const keyz = {
   KeyW:false,
   KeyZ:false
 };
+
+canvas.addEventListener('click', startGame);
+
+function startGame() {
+  cancelAnimationFrame(game.req);
+  players.forEach((player) => {
+    player.score = 0;
+    player.cooldown = 100;
+    player.speed = Math.ceil(grid / 8);
+    player.size = grid / 2 + 5;
+    player.y = canvas.height / 2;
+  })
+  game.req = requestAnimationFrame(draw);
+}
+
+// Fire bullets - space bar and D key
 document.addEventListener('keydown', (e)=> {
   if(e.code in keyz) {
     keyz[e.code] = true;
@@ -43,34 +81,64 @@ document.addEventListener('keydown', (e)=> {
   if(e.code == 'Space' && players[0].cooldown <= 0) {
     players[0].cooldown = 20;
     game.bullets.push({
-      x:players[0].x - players[0].size -15,
-      y:players[0].y -5,
+      x:players[0].x - players[0].size - 15,
+      y:players[0].y - 5,
       speed: -game.bulletSpeed,
       size:10,
       color:'indianred',
     })
   }
   if(e.code == 'KeyD' && players[1].cooldown <= 0) {
+    
     players[1].cooldown = 20;
     game.bullets.push({
-      x:players[1].x - players[1].size +15,
-      y:players[1].y -5,
+      x: players[1].x + players[1].size + 15,
+      y: players[1].y - 5,
       speed: game.bulletSpeed,
-      size:10,
-      color:'royalblue',
+      size: 10,
+      color: 'royalblue',
     })
   }
 })
+
 document.addEventListener('keyup', (e)=> {
   if(e.code in keyz) {
     keyz[e.code] = false;
   }
-  console.log(keyz);
+  if(e.code == 'KeyM') {
+    console.log(players[0].x, players[0].y);
+    console.log(players[1].x, players[1].y);
+    colDec(players[0], players[1]);
+  }
 })
 
-game.req = requestAnimationFrame(draw);
+
+
+function colDec(a, b) {
+  return a.x < b.x + b.size && a.x + a.size * 2 > b.x && a.y < b.y - b.size + b.size * 2 && a.y + a.size * 2 > b.y - b.size;
+}
 
 function movementPlayer() {
+if (mind.thinking) {
+  if(mind.count > 0) {
+    mind.count--;
+  } else {
+    let val = Math.floor(Math.random() * 20);
+    mind.count = 10;
+    if (players[1].y + val < players[0].y) {
+    players[1].y += players[1].speed;
+    } else if (players[1].y + val > players[0].y) {
+    mind.dir = -players[1].speed;
+    } 
+  }
+  players[1].y += mind.dir;
+  // if (players[1].y + val < players[0].y) {
+  //   players[1].y += players[1].speed;
+  // } else if (players[1].y + val > players[0].y) {
+  //   players[1].y -= players[1].speed;
+  // }
+}
+
   if(keyz['ArrowLeft'] && players[0].x > canvas.width/2+players[0].size) {
     players[0].x -= players[0].speed;
   }
@@ -98,26 +166,41 @@ function movementPlayer() {
 }
 
 function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   movementPlayer();
-  game.bullets.forEach((bull,index)=>{
+  game.bullets.forEach((bull, index) => {
     ctx.fillStyle = bull.color;
-    ctx.fillRect(bull.x,bull.y,bull.size,bull.size);
-    bull.x+=bull.speed;
-    if(bull.x<0) {
-      game.bullets.splice(index,1);
+    ctx.fillRect(bull.x + (bull.size/2), bull.y, bull.size, bull.size);
+    bull.x += bull.speed;
+    if (bull.x < 0) {
+      game.bullets.splice(index, 1);
     }
+    players.forEach((player, i) => {
+      if(colDec(bull, player)) {
+      console.log('HIT player ' + player.color + ' ' + i);
+      if(i==0) {
+        players[1].score++;
+      } else {
+        players[0].score++;
+      }
+      game.bullets.splice(index, 1);
+      }
+    })
   })
   ctx.beginPath();
-  ctx.moveTo(canvas.width/2,0);
-  ctx.lineTo(canvas.width/2,canvas.height);
+  ctx.moveTo(canvas.width/2, 0);
+  ctx.lineTo(canvas.width/2, canvas.height);
   ctx.stroke();
   players.forEach((player)=>{
     if(player.cooldown > 0) {
       player.cooldown--;
     }
-    ctx.beginPath();
     ctx.fillStyle = player.color;
+    ctx.font = grid + 'px serif';
+    ctx.textAlign = 'center';
+    ctx.fillText('Score:' + player.score, player.pos, grid);
+    ctx.beginPath();
+    
     ctx.arc(player.x,player.y,player.size,0,Math.PI*2);
     ctx.fill();
   })
